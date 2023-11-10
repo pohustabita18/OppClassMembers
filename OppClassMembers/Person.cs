ffef648d80e47fc32b25fbd9513e9167fec25168
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2023-11-09 19:28:06 -1200

    example with readonly fields

---
+++ OppClassMembers/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace OppClassMembers
+{
+    internal class Person
+    {
+        public string FirstName = "John";
+
+        public string LastName = "Doe";
+
+        // readonly field can be modified from the initialization expression (#1)
+        public readonly string CNP = "1234";
+
+        public static int PopulationCount = 0;
+
+        public static readonly int MaxLifespanYears = IntiMaxLifespan();
+
+        // acesta este un constructor
+        public Person(string cnp)
+        {
+            //readonly poate fii modificat from the constructor(#2)
+            CNP =cnp;
+
+        }
+
+        public void PrintPerson()
+        {
+           
+        }
+
+        public static void PrintPopulationCount()
+        {
+
+        }
+
+        public static int IntiMaxLifespan()
+        {
+            Console.Write("Max Lifespan=");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+}
---
+++ OppClassMembers/Program.cs
@@ -0,0 +1,21 @@
+﻿using OppClassMembers;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int IntiMaxLifespan = Person.MaxLifespanYears;
+
+            Person p = new Person("2344");
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.CNP);
+
+           // Person.MaxLifespanYears
+           //p.CNP="2344";
+            Person.PopulationCount = 1;
+        }
+    }
+}
